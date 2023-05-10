973c4bc7a8846698e8172cf2e733adfaa555b6ec
Ari-Ario <xusrew.z@gmail.com>
2023-05-10 14:09:55 +0200

    main layout created

--- calcul2.py
+++ calcul2.py
@@ -7,7 +7,26 @@ from kivy.uix.textinput import TextInput
 class CalculatorApp(App):
     def build(self):
         main_layout= BoxLayout(orientation="vertical")
+        self.text_box= TextInput(font_size= 30)
+        main_layout.add_widget(self.text_box)
+        buttons = [
+            ["7", "8", "9", "/"],
+            ["4", "5", "6", "*"],
+            ["1", "2", "3", "-"],
+            [".", "0", "Clear", "+"],
+            ]
 
+        for row in buttons:
+            y_layout= BoxLayout(spacing=5)
+            for i in row:
+                butt=Button(text=f"{i}")
+                #butt.bind(on_press=self.write)
+                y_layout.add_widget(butt)
+            main_layout.add_widget(y_layout)
+        equal_butt= Button(text= "=")
+        #equal_butt.bind(on_press= self.solve)
+        main_layout.add_widget(equal_butt)
+        
         return main_layout
     
 if __name__== "__main__":
